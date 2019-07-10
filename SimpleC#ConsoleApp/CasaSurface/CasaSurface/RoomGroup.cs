6ba095a002fcf93340ab17f5aea84d6cdc3bb322
samuelkennett <sjk7885@gmail.com>
2019-07-10 11:53:05 -0400

    RoomGroup Datastructure implementation
    I created 3 classes here. FirstFloor and SecondFloor, which initilizes a list of rooms with a GroupID string (i.e. "1st Floor"), and a RoomGroup Class who's only class member is a Dictionary object that maps the GroupID string to the a list rooms. The RoomGroup Class initializes the FirstFloor and SecondFloor classes, thus creating the list of rooms based upon which floor they're on and their respective GroupIDs.

---
+++ SimpleC#ConsoleApp/CasaSurface/CasaSurface/RoomGroup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CasaSurface
+{
+    public class RoomGroup
+    {
+
+        Dictionary<string, List<Room>> m_dRoomGroup;
+
+        public RoomGroup()
+        {
+            InitializeRoomGroup();
+        }
+
+        public void InitializeRoomGroup()
+        {
+            Dictionary<string, List<Room>> dRoomGroup = new Dictionary<string, List<Room>>();
+            FirstFloor flFirstFloor = new FirstFloor();//Initializes First Floor rooms
+            SecondFloor flSecondFloor = new SecondFloor();//Initializes Second Floor rooms
+            dRoomGroup.Add(flFirstFloor.GetGroupID(), flFirstFloor.GetRooms());//maps GroupID "1st Floor" to the list of rooms in FirstFloor class
+            dRoomGroup.Add(flSecondFloor.GetGroupID(), flSecondFloor.GetRooms());//maps GroupID "2nd Floor" to the list of rooms in FirstFloor class
+            SetRoomGroup(dRoomGroup);
+        }
+
+        public void SetRoomGroup(Dictionary<string, List<Room>> dRoomGroup)
+        {
+            this.m_dRoomGroup = dRoomGroup;
+        }
+
+        public Dictionary<string, List<Room>> GetRoomGroupDictionary()//Dictionary that is accessible from the main class.
+        {
+            return this.m_dRoomGroup;
+        }
+    } 
+}
---
+++ SimpleC#ConsoleApp/CasaSurface/CasaSurface/SecondFloor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaSurface
+{
+    public class SecondFloor
+    {
+        string m_strGroupID; //RoomGroup Dictionary Key 
+        List<Room> m_lFirstFloorRooms;//RoomGroup Dictionary Value
+
+        public SecondFloor()
+        {
+            m_strGroupID = "2nd Floor";
+            InitializeRooms();
+        }
+
+        private void SetRooms(List<Room> lFirstFloorRooms)
+        {
+            this.m_lFirstFloorRooms = lFirstFloorRooms;
+        }
+
+        public List<Room> GetRooms()
+        {
+            return this.m_lFirstFloorRooms;
+        }
+
+        private void InitializeRooms()//creates room objects with only the room number explicitly set
+        {
+            List<Room> lRooms = new List<Room>();
+            Room room201 = new Room("201");
+            Room room202 = new Room("202");
+            Room room203 = new Room("203");
+            Room room204 = new Room("204");
+            Room room205 = new Room("205");
+            Room room206 = new Room("206");
+            Room room207 = new Room("207");
+            Room room208 = new Room("208");
+            Room room209 = new Room("209");
+            Room room210 = new Room("210");
+            Room room211 = new Room("211");
+            Room room212 = new Room("212");
+            lRooms.Add(room201);
+            lRooms.Add(room202);
+            lRooms.Add(room203);
+            lRooms.Add(room204);
+            lRooms.Add(room205);
+            lRooms.Add(room206);
+            lRooms.Add(room207);
+            lRooms.Add(room208);
+            lRooms.Add(room209);
+            lRooms.Add(room210);
+            lRooms.Add(room211);
+            lRooms.Add(room212);
+            SetRooms(lRooms);
+        }
+
+        public string GetGroupID()
+        {
+            return this.m_strGroupID;
+        }
+    }
+}
